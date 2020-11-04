1772b85a46a5272ebb3dd652d60eb02190faaa92
Dario <dario.vra123@gmail.com>
2020-11-04 13:30:19 +0100

    Implement initial login logic

---
+++ postgres-express-node/api/controllers/login.controller.js
@@ -0,0 +1,19 @@
+const winston = require("winston");
+const { loginServiceInstance } = require("../../services");
+
+const Logger = winston.loggers.get("logger");
+
+exports.login = async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = await loginServiceInstance.getUser({ username });
+    
+    if(user && user.password === password){
+      return res.json({message: "Login successful", token: "auth_token"});
+    }
+    return res.status(401).json({ error: "Unauthorized" });
+  } catch (err) {
+    Logger.error(err);
+    return res.status(400).json({ error: { message: err.message } });
+  }
+};
